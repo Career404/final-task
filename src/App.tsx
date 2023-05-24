44d650a0780ed6bfb665d64bf49e272abab5b021
Career404 <105638831+Career404@users.noreply.github.com>
2023-05-24 21:30:11 +0400

    feat: add welcome page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import bgdna from './assets/bg.png';
 import './App.css';
 
 export default function App() {
 	return (
-		<>
-			<h1>{'Your final task'}</h1>
-		</>
+		<div
+			className="fullscreen bg-dna"
+			style={{
+				background: `url(${bgdna}) no-repeat`,
+				backgroundSize: 'cover',
+			}}
+		>
+			<div className="welcome">
+				<h1>Q-1 Search</h1>
+				<p>Please login to search through UniProt database</p>
+				<button>Login</button>
+			</div>
+		</div>
 	);
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,17 @@ import './index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App';
+import { createRoutesFromElements, Route } from 'react-router-dom';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+);
 
 ReactDOM.createRoot(document.querySelector('#root') as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={router} />
 	</React.StrictMode>
 );
